4a996c8fafc023cbc6e14cb0d0369e4a2780ea72
Amrit478 Dhillon <Amritraj4786@gmail.com>
2024-10-21 19:58:01 +0000

    first

--- src/main/java/com/banking/banking_backend/controller/UserController.java
+++ src/main/java/com/banking/banking_backend/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.banking.banking_backend.controller;
 
+import java.lang.annotation.Inherited;
+
+import javax.annotation.processing.Generated;
+
+@Entity
 public class UserController {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
 }
