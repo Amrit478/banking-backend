4996f0b10769664195d448a491085d7697c8737e
Amrit478 <Amritraj4786@gmail.com>
2024-10-24 19:34:21 -0400

    nft

--- src/main/java/com/banking/banking_backend/repository/UserRepository.java
+++ src/main/java/com/banking/banking_backend/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.banking.banking_backend.repository;public interface UserRepository {
+package com.banking.banking_backend.repository;
+
+import com.banking.banking_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
