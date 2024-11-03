a5c61f5dfcbd6e2f8fc5e29893c6ebc9b649f552
Amrit478 <Amritraj4786@gmail.com>
2024-11-03 07:09:04 -0500

    Working with Postman

--- src/main/java/com/banking/banking_backend/repository/UserRepository.java
+++ src/main/java/com/banking/banking_backend/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.banking.banking_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+//@Repository
+//
+//Marks a class as a Data Access Object (DAO) that interacts with the database.
+//Database access using by application.properties
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByIdAndName(int id, String name);
 }
