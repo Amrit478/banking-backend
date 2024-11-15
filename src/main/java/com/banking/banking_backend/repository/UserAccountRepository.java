5908baa22303c5961618f46fe4c929f21502a5c2
Amrit478 <Amritraj4786@gmail.com>
2024-11-15 15:35:56 -0500

    new files

--- src/main/java/com/banking/banking_backend/repository/UserAccountRepository.java
+++ src/main/java/com/banking/banking_backend/repository/UserAccountRepository.java
@@ -1,9 +1,9 @@
 package com.banking.banking_backend.repository;
 import com.banking.banking_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByIdAndName(int id, String name);
-    User findByUsername(String username);
+public interface UserAccountRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
